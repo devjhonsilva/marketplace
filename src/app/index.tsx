500be36d08f1e5acf3bedf096aa5c3aad519fb44
Jonatan Santos <jonatansilvasantos@outlook.comm>
2026-01-14 08:53:28 -0300

    Configuração de rotas públicas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,5 @@
-import { router } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Redirect } from "expo-router";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Nova tela.</Text>
-      <TouchableOpacity onPress={() => router.push("login")}>
-        <Text className="font-bold text-xl text-success ">Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Redirect href={"/login"} />;
 }
