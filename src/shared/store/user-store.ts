b6c6f8adf7e2f5188220b464542c41726e3f325a
Jonatan Santos <jonatansilvasantos@outlook.comm>
2026-01-16 08:31:49 -0300

    Adicionado o async storage e implementado para persistencia dos dados após login.

--- src/shared/store/user-store.ts
+++ src/shared/store/user-store.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { UserInterface } from "../interfaces/http/user";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface SetSessionParams {
   userData: UserInterface;
@@ -23,16 +25,29 @@ export interface UserStore {
   updateTokens: (updateTokensData: UpdateTokensParams) => void;
 }
 
-export const useUserStore = create<UserStore>()((set) => ({
-  user: null,
-  token: null,
-  refreshToken: null,
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      refreshToken: null,
 
-  logout: () => {},
-  setSession: (sessionData) => {
-    set({ ...sessionData });
-  },
-  updateTokens: (updateTokensData) => {
-    set({ ...updateTokensData });
-  },
-}));
+      logout: () =>
+        set({
+          user: null,
+          token: null,
+          refreshToken: null,
+        }),
+      setSession: (sessionData) => {
+        set({ ...sessionData });
+      },
+      updateTokens: (updateTokensData) => {
+        set({ ...updateTokensData });
+      },
+    }),
+    {
+      name: "marketplace-auth",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
